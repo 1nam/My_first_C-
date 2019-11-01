f96f105e8d82f315833303513017571caab0c9b7
zeus <33592162+1nam@users.noreply.github.com>
2019-11-01 04:09:49 -0400

    Create First_c_Sharp.cs

---
+++ First_c_Sharp.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		Console.Write("hi whats you name?: ");
+		string name = Console.ReadLine();
+		Console.WriteLine("Hello there: " + name);
+		Console.WriteLine("thats a pretty name: " + name + " is that French??");
+		{
+		}
+
+		Console.Write("so how old are you?: ");
+		string age = Console.ReadLine();
+		Console.WriteLine("ahh your not that old sweet heart.: " + age);
+		{
+		}
+
+		Console.Write("do you come here often?: ");
+		string x = Console.ReadLine();
+		Console.WriteLine(x + " haha me to.: ");
+	}
+}
